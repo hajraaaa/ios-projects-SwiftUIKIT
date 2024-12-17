41af148cec4559d7149e27344f3ae5d055a6fb08
Hajra Masood <hajra.masood@codesorbit.com>
2024-12-17 12:43:46 +0500

    Added scroll view project

---
+++ Scroll-View/Scroll View/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
